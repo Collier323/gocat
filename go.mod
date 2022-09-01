7708811db4fa9c092e85fc239c9a3dd5d3f8c95e
Collier323 <unwell4@gmail.com>
2022-09-01 00:45:47 -0500

    Added basic functionality

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module gocat
+module github.com/Collier323
 
 go 1.19
+
+require syreclabs.com/go/faker v1.2.3
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+// This only works partially at the moment.
+// todo: Add functionality read a file's contents and print them out with funny colors
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"time"
+)
+
+type Color struct {
+	a, b, c int
+}
+
+func main() {
+	info, _ := os.Stdin.Stat()
+	if info.Mode()&os.ModeCharDevice != 0 {
+		fmt.Println("The command is intended to work with pipes")
+		fmt.Println("Usage: Print funny colors")
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		// Main loop. Each iteration a new 'random' is created and will generate a random color for each rune.
+		s1 := rand.NewSource(time.Now().UnixNano())
+		s2 := rand.New(s1)
+		rgb := Color{s2.Intn(250), s2.Intn(250), s2.Intn(250)}
+
+		input, _, err := reader.ReadRune()
+		if err != nil && err == io.EOF {
+			break
+		}
+		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", rgb.a, rgb.b, rgb.c, input)
+	}
+}
+
+//	var phrases []string
+//	for i := 1; i < 3; i++ {
+//		phrases = append(phrases, faker.Hacker().Phrases()...)
+//	}
